3176b7f9001eef265310f68d68d3256b1d87033d
tobypenguin <pantsvszomebie@gmail.com>
2023-03-02 17:35:00 +0700

    [.] Update validation of the field

--- src/entities/schemas/account.schema.js
+++ src/entities/schemas/account.schema.js
@@ -3,32 +3,51 @@ const Schema = mongoose.Schema;
 
 let StudentSchema = new Schema(
   {
-    _id: { 
-      type: mongoose.Types.ObjectId, 
-      auto: true, 
-      autoIndex: true 
+    _id: {
+      type: mongoose.Types.ObjectId,
+      auto: true,
     },
     firstName: {
       type: String,
+      required: true,
     },
     lastName: {
       type: String,
+      required: true
     },
     nickName: {
       type: String,
+      required: true,
     },
     phone: {
-        type: String,
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /\d{3}-\d{3}-\d{4}/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
       },
-    age: {
-        type: Number,
-      },
-    birthDate: {
-        type: String,
+
     },
-  }, 
+    age: {
+      type: Number,
+      required: true
+    },
+    birthDate: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/.test(v);
+        },
+        message: props => `${props.value} is not a valid birth date!`
+      },
+    },
+  },
   { timestamps: true },
   { collection: "student" },
+  {runValidators: true},
 );
 
 const Account = mongoose.model("student", StudentSchema);
--- src/repositories/account.repository.js
+++ src/repositories/account.repository.js
@@ -1,26 +1,65 @@
 const Account = require('../entities/schemas/account.schema');
+const QueueRepository = require('./queue.repository');
 
 class AccountRepository {
+  constructor(queueUrl, exchangeName) {
+    this.queueRepository = new QueueRepository(queueUrl, exchangeName);
+  }
+
   async create(account) {
     const newAccount = new Account(account);
-    await newAccount.save();
-    return newAccount.toObject();
+    try {
+      await newAccount.save();
+      const message = JSON.stringify({
+        type: 'create',
+        account: newAccount.toObject(),
+      });
+      const routingKey = 'create';
+      await this.queueRepository.sendMessage(message, routingKey);
+
+      return newAccount.toObject();
+    } catch (err) {
+      return ({ error: err.message })
+    }
   }
 
   async getById(id) {
-    const account = await Account.findById(id).lean();
-    return account;
+    try {
+      const account = await Account.findById(id).lean();
+      return account;
+    } catch (err) {
+      return ({ error: err.message })
+    }
   }
 
   async update(account) {
-    const updatedAccount = await Account.findByIdAndUpdate(account.id, account, {
-      new: true,
-    }).lean();
-    return updatedAccount;
+    try {
+      const updatedAccount = await Account.findOneAndUpdate(account.id, account, {
+        new: true,
+        runValidators: true
+      }).lean();
+      const message = JSON.stringify({
+        type: 'update',
+        account: updatedAccount,
+      });
+      const routingKey = 'update';
+      await this.queueRepository.sendMessage(message, routingKey);
+
+      return updatedAccount;
+    } catch (err) {
+      return ({ error: err.message })
+    }
   }
 
   async delete(id) {
     const deletedAccount = await Account.findByIdAndDelete(id).lean();
+    const message = JSON.stringify({
+      type: 'delete',
+      account: deletedAccount,
+    });
+    const routingKey = 'delete';
+    await this.queueRepository.sendMessage(message, routingKey);
+
     return deletedAccount;
   }
 }
